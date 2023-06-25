a475ad46fb5ab8be04e02d6d8b83973abe14f80c
a.khabbazan <a.khabbazan@hamafza-startup.ir>
2023-06-25 18:46:16 +0330

    initial django-graphene

--- Goodreads/settings.py
+++ Goodreads/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# GraphQL Settings
+GRAPHENE = {
+    "SCHEMA": "Goodreads.schema.schema"
+}
